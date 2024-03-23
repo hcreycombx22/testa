52ded3ebeeb91db25e6171f069d1b2e50543e288
hcreycombx22 <164627575+hcreycombx22@users.noreply.github.com>
2024-03-23 19:09:36 +0700

    Add files via upload

---
+++ cc.lua
@@ -0,0 +1,19 @@
+local gameId = game.PlaceId
+
+local function getServer()
+    local servers = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. tostring(gameId) .. '/servers/Public?sortOrder=Asc&limit=10')).data
+    local server = servers[Random.new():NextInteger(80, 100)]
+    if server then
+        return server
+    else
+        return getServer()
+    end
+end
+
+game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, getServer().id, game.Players.LocalPlayer)
+
+task.wait(10)
+while true do
+    game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+    task.wait()
+end
